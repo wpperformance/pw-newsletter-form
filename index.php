ba4303e4907e313221e82f2b163b0dff42e630de
ipatate <faramazpat@gmail.com>
2025-07-31 10:15:00 +0200

    Update plugin metadata and styles for PW Newsletter Form

--- index.php
+++ index.php
@@ -2,33 +2,32 @@
 
 namespace PWNewsletterForm;
 
-/*
-Plugin Name: PW Newsletter Form
-Plugin URI:
-Description: Newsletter form plugin connected to Brevo
-Version: 0.0.2
-Tested up to: 6.3
-Requires PHP: 8.1
-Author: Patrick Faramaz
-Author URI: https://wp-performance.com
-
-GitHub Plugin URI: https://github.com/wp-performance/pw-newsletter-form
-
-Copyright 2025 Patrick Faramaz
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2, as
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+/**
+ * Plugin Name: PW Newsletter Form
+ * Description: Newsletter form plugin connected to Brevo
+ * Version: 0.0.3
+ * Tested up to: 6.8
+ * Requires PHP: 8.1
+ * Author: Patrick Faramaz
+ * Author URI: https://wp-performance.com
+ * Text Domain: pw-newsletter-form
+ * GitHub Plugin URI: https://github.com/wp-performance/pw-newsletter-form
+ *
+ * Copyright 2025 Patrick Faramaz
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ **/
 
 if (!defined('ABSPATH')) {
   exit; // Exit if accessed directly.
